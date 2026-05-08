237b31b28fb2751bb2083710ee627f1ae002a25d
xialinxi <2360165050@qq.com>
2026-05-08 23:18:44 +0800

    dev: test  homework 1 insert sort

--- exercises/01_insert_sort/01_insert_sort.c
+++ exercises/01_insert_sort/01_insert_sort.c
@@ -7,10 +7,18 @@ typedef struct {
     int score;
 } Student;
 
+
+// 插入排序函数，按成绩从高到低排序
 void insertion_sort(Student students[], int n) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    // this is test qemu repo 
+    for (int i = 1; i < n; i++) {
+        Student key = students[i];
+        int j = i - 1;
+        while (j >= 0 && students[j].score < key.score) {
+            students[j + 1] = students[j];
+            j--;
+        }
+        students[j + 1] = key;
+    }
 }
 
 int main(void) {
